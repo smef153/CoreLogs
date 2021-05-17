2060eb93132f6a8b89dad19a8015f761ed54366a
Oliver <74798507+smef153@users.noreply.github.com>
2021-05-17 11:01:14 +1000

    CoreLogs V1.1

---
+++ CoreLogs/config.lua
@@ -0,0 +1,14 @@
+-- Leave/Join Discord Log Config
+
+Config = {}
+
+Config.playerID = true				
+Config.steamID = true				
+Config.steamURL = true				
+Config.discordID = true
+Config.xblID = true	
+Config.liveID = true
+Config.licenseID = true
+
+
+Config.Logo = "insert image link here"	
---
+++ CoreLogs/fxmanifest.lua
@@ -0,0 +1,16 @@
+author 'Peter.#0766'
+description 'Core logs By Peter.#0766'
+version '1.0'
+game 'gta5'
+fx_version 'bodacious'
+
+server_script {
+"server.lua",
+"config.lua"
+}
+
+client_script 'client.lua'
+
+
+
+
